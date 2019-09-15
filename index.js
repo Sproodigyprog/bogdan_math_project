afb04ab2f1f97f4a05c647095e5a00e3fad7a370
Sproodigy <sproodigy@gmail.com>
2019-09-15 17:20:23 +0400

    Changed server port

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.set('port', process.env.PORT || 8080);
 
 app.get('/', function(req, res) {
   res.render('main');
